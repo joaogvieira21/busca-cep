cd1fcb7d4d2f6ad4d49efbeaeea231b418f1b32e
João Gabriel Vieira <joaogvieira21@gmail.com>
2023-01-31 16:28:50 -0300

    Versão 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import { FiSearch } from 'react-icons/fi'
-
+import { useState} from 'react'
 import './App.css';
 import React from 'react'
+import api from './services/api'
 
 function App() {
+
+  const [inputCep, setInputCep] = useState("");
+  const [cep, setCep] = useState({});
+
+
+  async function handleSearch () {
+    if(inputCep.length < 8) {
+      alert("Digite um cep com 8 números!")
+      setInputCep("");
+    }
+    try{
+      const response = await api.get(`${inputCep}/json`)
+      setCep(response.data)
+      setInputCep("");
+    }catch{
+
+    }
+  }
   return( 
     <div className="container">
       <h1>Buscador de CEP</h1>
       <div className="input-container">
-        <input size="25" placeholder="Digite o cep..."></input>
-        <button className="btn">
+        <input size="25" placeholder="Digite o cep..." value={inputCep} onChange={(e)=>setInputCep(e.target.value)}></input>
+        <button className="btn" onClick={handleSearch}>
           <FiSearch size="25" ></FiSearch>
         </button>
       </div>
       <main className="show-cep">
-        <h2>CEP: 88111-675</h2>
-        <span>Rua Honoria Virgilina</span>
-        <span>Complemento: qualquer</span>
-        <span>Real Parque</span>
-        <span>São José - SC</span>
+        <h2>CEP: {cep.cep} </h2>
+        <span>{cep.logradouro}</span>
+        <span>{cep.bairro}</span>
+        <span>{cep.localidade}</span>
       </main>
     </div>
     )
